de7766fb39cd63debf0f9c033dfa9097f57109f4
mmbannert <michael.bannert@uni-tuebingen.de>
2026-03-31 15:32:22 +0000

    Deploying to gh-pages from @ mmbannert/mmbannert.github.io@198e4af129e6a4549bc9677f8aa1466d20a3a30d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,35 +518,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%61%69%6C@%6D%69%63%68%61%65%6C-%62%61%6E%6E%65%72%74.%64%65", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://orcid.org/0000-0003-1010-7517", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-stackoverflow',
+        title: 'Stackoverflow',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://stackoverflow.com/users/13440784", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-osf',
+        title: 'Open Science Framework',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://osf.io/utkd5/", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://github.com/mmbannert", "_blank");
         },
       },{
       id: 'light-theme',
