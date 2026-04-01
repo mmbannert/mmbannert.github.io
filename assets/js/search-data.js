cccca546a4c3b16a291f4318df4dd9a00b2e64c4
mmbannert <michael.bannert@uni-tuebingen.de>
2026-04-01 13:09:30 +0000

    Deploying to gh-pages from @ mmbannert/mmbannert.github.io@ae991a34f1a245a34cdcfa228ca248b0d738574f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "When the mood shakes me, I contribute code to libraries that I work with. See below for a selection of relevant projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
